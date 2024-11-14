8c75b8c33c4d4465135b4b15cc9e3baf4de694a3
Vladimir Ovechkin <vovechkin@ecom.tech>
2024-11-14 20:48:57 +0300

    feat(core): implement basic pdf viewer and demo
    Implemented pdf viewer class and html+js demo

--- examples/core/src/index.ts
+++ examples/core/src/index.ts
@@ -1,5 +1,38 @@
-import { corePrint,  } from '@pdf-viewer-toolkit/core';
-import { modulePrint } from '@pdf-viewer-toolkit/core/module';
+import { PDFViewer } from "@pdf-viewer-toolkit/core";
 
-corePrint();
-modulePrint();
+const viewer = new PDFViewer({
+  url: "light_pdf.pdf",
+  canvas: document.getElementById("the-canvas") as HTMLCanvasElement,
+});
+
+async function main() {
+  await viewer.init();
+
+  const drawPageNumber = () => {
+    document.getElementById("page_num")!.textContent =
+      viewer.currentPage.toString();
+  };
+
+  const onPrevPage = () => {
+    if (viewer.currentPage === 1) {
+      return;
+    }
+    viewer.renderPage(viewer.currentPage - 1);
+    drawPageNumber();
+  };
+
+  const onNextPage = () => {
+    if (viewer.currentPage === viewer.pageCount) {
+      return;
+    }
+    viewer.renderPage(viewer.currentPage + 1);
+    drawPageNumber();
+  };
+
+  drawPageNumber();
+  document.getElementById("page_count")!.textContent = viewer.pageCount.toString();
+  document.getElementById("prev")?.addEventListener("click", onPrevPage);
+  document.getElementById("next")?.addEventListener("click", onNextPage);
+}
+
+main();
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,1 @@
-// import { print } from '@pdf-viewer-toolkit/utilities';
-
-export const corePrint = () => {
-  // print('core');
-  return 'core';
-};
+export { PDFViewer } from './PDFViewer';
