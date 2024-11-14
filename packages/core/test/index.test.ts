73eb55cd1ea43b49ddf30f7025833f2d75a8e86c
Vladimir Ovechkin <ovchingus@gmail.com>
2024-11-14 20:48:57 +0300

    feat(core): implement basic pdf viewer and demo
    Implemented pdf viewer class and html+js demo

--- packages/core/test/index.test.ts
+++ packages/core/test/index.test.ts
@@ -1,23 +1,11 @@
-import { afterEach, describe, expect, it, jest } from '@jest/globals';
-import { corePrint } from '@pdf-viewer-toolkit/core';
-import { modulePrint } from '@pdf-viewer-toolkit/core/module';
+import { afterEach, describe, it, jest } from '@jest/globals';
 
 afterEach(() => {
   jest.clearAllMocks();
 });
 
-describe('corePrint', () => {
-  it('Check to see if the log outputs correctly.', () => {
-    const result = corePrint();
-
-    expect(result).toBe('core');
-  });
-});
-
-describe('modulePrint', () => {
-  it('Check to see if the log outputs correctly.', () => {
-    const result = modulePrint();
-
-    expect(result).toBe('module');
+describe('PDFViewer', () => {
+  it('should create a PDF viewer', async () => {
+    // TODO: tests
   });
 });
